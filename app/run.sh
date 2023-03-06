3ecc6080ec4a6b99c834a15c17cf9afaffedbc33
Jp <joe.pentland@gmail.com>
2023-03-06 23:15:15 +0100

    run.sh: Remove mount of GCS (use local storage instead)

--- app/run.sh
+++ app/run.sh
@@ -4,10 +4,6 @@ set -eo pipefail
 # Create mount directory for service.
 mkdir -p $MNT_DIR
 
-echo "Mounting GCS Fuse."
-gcsfuse --debug_gcs --debug_fuse $BUCKET $MNT_DIR 
-echo "Mounting completed."
-
 # Run the web service on container startup. Here we use the gunicorn
 # webserver, with one worker process and 8 threads.
 # For environments with multiple CPU cores, increase the number of workers
